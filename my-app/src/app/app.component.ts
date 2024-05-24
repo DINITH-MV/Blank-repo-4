465d22d5a455fd5d7e96b27556e99ec4913779fb
DINITH-MV <132985087+DINITH-MV@users.noreply.github.com>
2024-05-25 01:35:27 +0530

    creating a component

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MainComponent } from './main/main.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  providers: [MainComponent] // Add MainComponent as a provider
 })
 export class AppComponent {
   title = 'my-app';
@@ -15,4 +17,6 @@ export class AppComponent {
   name = "Angular"
   age = 10
 }
+
+
 
