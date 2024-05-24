7c1973f055b37fe47c8af5a50f479a188f52bbac
DINITH-MV <132985087+DINITH-MV@users.noreply.github.com>
2024-05-24 23:52:17 +0530

    Create main.component.ts

---
+++ my-app/src/app/main/main.component.ts
@@ -0,0 +1,11 @@
+@Component({
+    selector: 'app-root',
+    standalone: true,
+    imports: [RouterOutlet],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
+  })
+
+class mainComponent {
+
+}
