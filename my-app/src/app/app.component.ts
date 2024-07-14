a699c968843fa17dea32137b531eabc31cb7a09b
dinith vanderlan <dinithvanderlan@gmail.com>
2024-07-14 12:30:15 +0530

    updates

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NewComponent } from './new/new.component';
+import { MainComponent } from './main/main.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NewComponent],
+  imports: [RouterOutlet, NewComponent, MainComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
